ee18e46892abba6ff757748943ef420d36795430
camarada_aldo <camarada_aldo@yahoo.com.br>
2008-02-22 02:23:28 +0000

    httpserv: add a thread manager

--- dev/tools/lightwebserver/breaktokens.pp
+++ dev/tools/lightwebserver/breaktokens.pp
@@ -5,7 +5,15 @@ Interface
 Type
     TTokenList = Array[0..255] Of AnsiString;
 
+    TDecodedURI = Record
+      Document   : AnsiString;
+      Parameters : AnsiString;
+    End;
+
 Function BreakApart(LineText : AnsiString): TTokenList;
+Function DecodeURI(LineText : AnsiString): TDecodedURI;
+Function DecodeHeader_Label(LineText : AnsiString): AnsiString;
+Function DecodeHeader_Value(LineText : AnsiString): AnsiString;
 Function UnQuote(Line : AnsiString): AnsiString;
 Function RemoveDot(Ext : AnsiString): AnsiString;
 
@@ -39,6 +47,70 @@ Begin
         End;
     End;
     BreakApart := LineTokens;
+End;
+
+Function DecodeURI(LineText : AnsiString): TDecodedURI;
+Var
+  Ctrl : LongWord;
+  Temp : TDecodedURI;
+Begin
+  Temp.Document   := '';
+  Temp.Parameters := '';
+  Ctrl            := 1;
+  While Not(LineText[Ctrl] = '?')
+  And (Ctrl <= Length(LineText)) Do
+  Begin
+    Temp.Document := Temp.Document + LineText[Ctrl];
+    Inc(Ctrl);
+  End;
+  If LineText[Ctrl] = '?' Then
+  Begin
+    Inc(Ctrl);
+    While Ctrl <= Length(LineText) Do
+    Begin
+      Temp.Parameters := Temp.Parameters + LineText[Ctrl];
+      Inc(Ctrl);
+    End;
+  End;
+  DecodeURI := Temp;
+End;
+
+Function DecodeHeader_Label(LineText : AnsiString): AnsiString;
+Var
+  Ctrl : LongWord;
+  Temp : AnsiString;
+Begin
+  Ctrl := 1;
+  Temp := '';
+  While Not(LineText[Ctrl] = ':')
+  And (Ctrl <= Length(LineText)) Do
+  Begin
+    Temp := Temp + LineText[Ctrl];
+    Inc(Ctrl);
+  End;
+  DecodeHeader_Label := Temp;
+End;
+
+Function DecodeHeader_Value(LineText : AnsiString): AnsiString;
+Var
+  Ctrl : LongWord;
+  Temp : AnsiString;
+Begin
+  Ctrl := 1;
+  Temp := '';
+  While Not(LineText[Ctrl] = ':')
+  And (Ctrl <= Length(LineText)) Do
+    Inc(Ctrl);
+  If LineText[Ctrl] = ':' Then
+    Inc(Ctrl);
+  If LineText[Ctrl] = ' ' Then
+    Inc(Ctrl);
+  While Ctrl <= Length(LineText) Do
+  Begin
+    Temp := Temp + LineText[Ctrl];
+    Inc(Ctrl);
+  End;
+  DecodeHeader_Value := Temp;
 End;
 
 Function UnQuote(Line : AnsiString): AnsiString;
