41db33a4ab78baeb0bf3fddb706365c4c163af6c
adsense <adsense@z505.com>
2008-02-23 14:08:05 +0000

    build updates

--- dev/examples/build.pp
+++ dev/examples/build.pp
@@ -35,21 +35,21 @@ begin
   if (all) or (group = names[tGzipOn]) then begin
     AddDefine(o, 'GZIP_ON'); 
     o.Name:= group;
-    o.ProgBinDir:= 'bin-'+group; 
+    o.ProgBinDir:= 'bin-'+names[tGzipOn]; 
     CreateGroup(Paths,  o);
   end;
 
   if (all) or (group = names[tGzipSysutilsOn]) then begin
     AddDefine(o, 'SYSUTILS_ON');
     o.Name:= group;
-    o.ProgBinDir:= 'bin-'+group; 
+    o.ProgBinDir:= 'bin-'+names[tGzipSysutilsOn]; 
     CreateGroup(Paths,  o);
   end;
 
   if (all) or (group = names[tSysutilsOn]) then begin
     ResetDefines(o);
     AddDefine(o, 'SYSUTILS_ON');
-    o.ProgBinDir:= 'bin-'+group; 
+    o.ProgBinDir:= 'bin-'+names[tSysutilsOn]; 
     o.Name:= group;
     CreateGroup(Paths,  o);
   end;
@@ -63,12 +63,9 @@ var Paths: TPaths;
 begin
   // visible for HELP command
   SetVisibleGroups(names);
+  // suck em up
   GetDirFiles('./', '*.dpr', Paths);
-  if Paths.count < 0 then 
-  begin
-    writeln('problem FUCK');
-    halt;
-  end;
+  if Paths.count < 0 then HaltErr('Path problem getting example dpr files');
   BuildExamples(Paths);
 end.
 
