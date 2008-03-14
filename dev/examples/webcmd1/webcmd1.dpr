e2db0e8fe57b280c8dc8f6da53f6ed6f78d22a40
adsense <adsense@z505.com>
2008-03-14 11:43:43 +0000

    untested win compatibility

--- dev/examples/webcmd1/webcmd1.dpr
+++ dev/examples/webcmd1/webcmd1.dpr
@@ -26,6 +26,34 @@ begin
   out('<br><b>Error:</b> ' + s);
 end;
 
+{$ifdef windows}
+ { parse for program command (before first space) }
+ function GetCmdPath(const cmd: string): string;
+ var i: integer;
+ begin
+   result:= '';
+   if cmd = '' then exit;
+   for i:= 1 to length(cmd) do begin
+     if cmd[i] = ' ' then exit;
+     result:= result + cmd[i];
+   end;
+ end;
+ 
+ { parse for command arguments (after space) }
+ function GetCmdArgs(const cmd: string): string;
+ var i, spacefound: integer;
+ begin
+   result:= '';
+   spacefound:= 0;
+   if cmd = '' then exit;
+   for i:= 1 to length(cmd) do begin
+     if spacefound > 1 then result:= result + cmd[i];
+     if cmd[i] = ' ' then inc(spacefound);
+   end;
+ end;
+{$endif}
+
+
 procedure RunAndShowCmd(const cmd: string);
 var err : Longint;
 begin
@@ -33,7 +61,7 @@ begin
   out('Output of command: <b>'+ cmd + '</b>');
   outln('<textarea style="width:100%; font-size:0.9em; font-family:courier new;" ROWS=80>');
   {$ifdef unix}   err:= fpSystem(cmd);{$endif} //ls -l *.pp
-  {$ifdef windows}err:= executeprocess('', cmd);{$endif}
+  {$ifdef windows}err:= executeprocess(GetCmdPath(cmd), GetCmdArgs(cmd));{$endif}
   outln(  '-------------------------------------------------------------------------');
   outln(  'WEBCMD NOTE: command exited with status: ' + inttostr(err));
   outln('</textarea>');
