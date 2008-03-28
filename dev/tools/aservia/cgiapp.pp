4576cbd7c7cd0e9787247e9e6ff2b2dae23d9b0b
adsense@z505.com <adsense@z505.com@8899f2f9-8946-0410-bedb-cd5a53bd3c4f>
2008-03-28 12:43:55 +0000

    Small web server

---
+++ dev/tools/aservia/cgiapp.pp
@@ -0,0 +1,49 @@
+{$MODE OBJFPC}
+{$SMARTLINK ON}
+{$LONGSTRINGS ON}
+
+program cgiapp;
+begin
+  writeln('Content-type: text/html');
+  writeln;
+  writeln('<html>');
+  writeln('<body>');
+  writeln('<pre>');
+  writeln('Serving from seven to eleven every night,');
+  writeln('It really makes life a drag, I don''t think that''s right.');
+  writeln('I''ve really, really been the best of fools, I did what I could.');
+  writeln('''Cause I love you, baby,'); 
+  writeln('How I love you, darling, How I serve you, baby,');
+  writeln('How I love you, vel, little vel.');
+  writeln('But baby, Since I''ve Been Serving You.'); 
+  writeln('I''m about to lose my worried mind.');
+  writeln;
+  writeln('Everybody trying to tell me that you didn''t mean me no good.');
+  writeln('I''ve been trying, And, let me tell you,'); 
+  writeln('Let me tell you I really did the best I could.');
+  writeln('I''ve been serving from seven to eleven every night,');
+  writeln('I said It kinda makes my life a drag.');
+  writeln('And, that ain''t right...');
+  writeln('Since I''ve Been Serving You, I''m about to lose my worried mind.');
+  writeln;
+  writeln('Said I''ve been crying, my tears they fell like rain,');
+  writeln('Don''t you hear, Don''t you hear them falling,');
+  writeln('Don''t you hear, Don''t you hear them falling.');
+  writeln;
+  writeln('Do you remember, when I knocked upon your 80 door?');
+  writeln('I said you had the nerve to tell me you didn''t want me no more');
+  writeln('I hear door 80 slam,');
+  writeln('You must have one of them new fangled Apache backdoor man.');
+  writeln;
+  writeln('I''ve been serving from seven, seven, seven, to eleven every night,');
+  writeln('It kinda makes my life a drag...');
+  writeln('Baby, Since I''ve Been Serving You, I''m about to lose,'); 
+  writeln('I''m about lose to my worried mind.');
+  writeln;
+  writeln('Love,');
+  writeln;
+  writeln(' Aservia');
+  writeln('</pre>');
+  writeln('</body>');
+  writeln('</html>');
+end.
