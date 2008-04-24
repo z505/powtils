8df639818a9111aa4873429ca90e93c241b51250
adsense@z505.com <adsense@z505.com@8899f2f9-8946-0410-bedb-cd5a53bd3c4f>
2008-04-24 06:12:00 +0000

    better unix mime/config files

--- dev/tools/aservia/cgiapp.dpr
+++ dev/tools/aservia/cgiapp.dpr
@@ -1,49 +1,56 @@
-{$MODE OBJFPC}
-{$SMARTLINK ON}
-{$LONGSTRINGS ON}
+program cgiapp; {$ifdef fpc}{$MODE OBJFPC}{$H+}{$endif}
 
-program cgiapp;
+procedure sayln(s: string); overload;
 begin
-  writeln('Content-type: text/html');
-  writeln;
-  writeln('<html>');
-  writeln('<body>');
-  writeln('<pre>');
-  writeln('Serving from seven to eleven every night,');
-  writeln('It really makes life a drag, I don''t think that''s right.');
-  writeln('I''ve really, really been the best of fools, I did what I could.');
-  writeln('''Cause I love you, baby,'); 
-  writeln('How I love you, darling, How I serve you, baby,');
-  writeln('How I love you, vel, little vel.');
-  writeln('But baby, Since I''ve Been Serving You.'); 
-  writeln('I''m about to lose my worried mind.');
-  writeln;
-  writeln('Everybody trying to tell me that you didn''t mean me no good.');
-  writeln('I''ve been trying, And, let me tell you,'); 
-  writeln('Let me tell you I really did the best I could.');
-  writeln('I''ve been serving from seven to eleven every night,');
-  writeln('I said It kinda makes my life a drag.');
-  writeln('And, that ain''t right...');
-  writeln('Since I''ve Been Serving You, I''m about to lose my worried mind.');
-  writeln;
-  writeln('Said I''ve been crying, my tears they fell like rain,');
-  writeln('Don''t you hear, Don''t you hear them falling,');
-  writeln('Don''t you hear, Don''t you hear them falling.');
-  writeln;
-  writeln('Do you remember, when I knocked upon your 80 door?');
-  writeln('I said you had the nerve to tell me you didn''t want me no more');
-  writeln('I hear door 80 slam,');
-  writeln('You must have one of them new fangled Apache backdoor man.');
-  writeln;
-  writeln('I''ve been serving from seven, seven, seven, to eleven every night,');
-  writeln('It kinda makes my life a drag...');
-  writeln('Baby, Since I''ve Been Serving You, I''m about to lose,'); 
-  writeln('I''m about lose to my worried mind.');
-  writeln;
-  writeln('Love,');
-  writeln;
-  writeln(' Aservia');
-  writeln('</pre>');
-  writeln('</body>');
-  writeln('</html>');
+  write(s + #13#10);
+end;
+
+procedure sayln; overload;
+begin
+  sayln('');
+end;
+
+begin
+  sayln('Content-type: text/html');
+  sayln;
+  sayln('<html>');
+  sayln('<body>');
+  sayln('<pre>');
+  sayln('Serving from seven to eleven every night,');
+  sayln('It really makes life a drag, I don''t think that''s right.');
+  sayln('I''ve really, really been the best of fools, I did what I could.');
+  sayln('''Cause I love you, baby,'); 
+  sayln('How I love you, darling, How I serve you, baby,');
+  sayln('How I love you, vel, little vel.');
+  sayln('But baby, Since I''ve Been Serving You.'); 
+  sayln('I''m about to lose my worried mind.');
+  sayln;
+  sayln('Everybody trying to tell me that you didn''t mean me no good.');
+  sayln('I''ve been trying, And, let me tell you,'); 
+  sayln('Let me tell you I really did the best I could.');
+  sayln('I''ve been serving from seven to eleven every night,');
+  sayln('I said It kinda makes my life a drag.');
+  sayln('And, that ain''t right...');
+  sayln('Since I''ve Been Serving You, I''m about to lose my worried mind.');
+  sayln;
+  sayln('Said I''ve been crying, my tears they fell like rain,');
+  sayln('Don''t you hear, Don''t you hear them falling,');
+  sayln('Don''t you hear, Don''t you hear them falling.');
+  sayln;
+  sayln('Do you remember, when I knocked upon your 80 door?');
+  sayln('I said you had the nerve to tell me you didn''t want me no more');
+  sayln('I hear door 80 slam,');
+  sayln('You must have one of them new fangled Apache backdoor man.');
+  sayln;
+  sayln('I''ve been serving from seven, seven, seven, to eleven every night,');
+  sayln('It kinda makes my life a drag...');
+  sayln('Baby, Since I''ve Been Serving You, I''m about to lose,'); 
+  sayln('I''m about lose to my worried mind.');
+  sayln;
+  sayln('Love,');
+  sayln;
+  sayln(' Aservia');
+  sayln('</pre>');
+  sayln('</body>');
+  sayln('</html>');
 end.
