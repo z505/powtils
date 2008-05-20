52a40353be8afeca3704da4233ff34663a77bd6c
adsense@z505.com <adsense@z505.com@8899f2f9-8946-0410-bedb-cd5a53bd3c4f>
2008-05-20 17:41:12 +0000

    updating build utils

--- dev/main/pwbuildutil.pas
+++ dev/main/pwbuildutil.pas
@@ -1,6 +1,10 @@
-{ Useful for automating and building demos, projects, etc. Fpmake and fpcmake 
-  are not serving my simple needs. These build utilities are born due to my 
-  hatred toward GNU Make files (ugliest f*cking cow sh*t ever seen by man).
+{ PowBuild - utilities for automating builds/makes/packages
+
+  Useful for automating and building demos, projects, etc. 
+  
+  Fpmake and fpcmake are not serving my simple needs. These utilities are born 
+  due to my hatred toward GNU Make files (ugliest f*cking cow sh*t ever seen by 
+  man).
 
   Notes: 
     - df is an alias for "default" for quick command line typing
@@ -8,12 +12,15 @@
     - CleanBeforeRebuild can be set to force
 
   NOTE: these build utils don't always take advantage of "reusing" compiled
-  global PPU files as some "Make" utilties do. I've had "PPU Hell" and 
-  "PPU conflicts" when trying to reuse global PPU files (and some projects 
-  require regenerating .PAS sources with DEFINES and include file issues. Fpc 
-  has bugs too). Compilations may be slower if local copies of ppu units are 
-  generated - but I've rarely found super fast compilations to be that great
-  useful compared to say "CORRECT compilations" that reduce headaches.
+  global PPU files. Other "Make" utilties do. More of this will be implemented 
+  in the future. However, I've had "PPU Hell" and "PPU conflicts" when trying 
+  to reuse global PPU files (and some projects require regenerating .PAS 
+  sources with DEFINES and include file issues. Fpc has bugs too). Compilations 
+  may be slower if local ppu copies are generated - but I've rarely found super 
+  fast compilations to be that great compared to "CORRECT compilations" that 
+  reduce headaches! 
+  
+  With time... these utils will contain more features.
  
   Authors: L505
            http://z505.com
@@ -30,22 +37,22 @@ uses
   arrayfuncs;
 
 type 
-                                               
+  // this may change to more generic "TGroupOptions" or "TCompilerOptions in future if more compilers supported                                             
   TFpcOptions = record
-    Name: str15;   // group name
-    Dir,              // working directory
-    Crapdir,          // subdir for .o/.a/.ppu files (relative to working dir)
-    ProgBinFile,         // exe output file name
-    ProgBinDir: astr;    // exe output directory
-    SmartStrip,       // -XX -CX -Xs
+    Name: str15;        // group name
+    Dir,                // working directory
+    Crapdir,            // subdir for .o/.a/.ppu files (relative to working dir)
+    ProgBinFile,        // exe output file name
+    ProgBinDir: astr;   // exe output directory
+    SmartStrip,         // -XX -CX -Xs
     Compile,
     Rebuild,
     CleanBeforeRebuild,
-    IgnoreErr: boo;   // ignore compiler errors
+    IgnoreErr: boo;     // ignore compiler errors
     Extra: astr;                 
     FpcVersion: astr;
-    intern: record    // private, not for interface user to worry about
-      defines,                                                         
+    intern: record      // private, not for interface user to worry about
+      defines,                                                          
       incpaths, 
       unitpaths: AstrArray;
       craptargetdir: astr;   // i.e. /.crap/i386-win32/
@@ -108,9 +115,7 @@ procedure SetVisibleGroups(const names: str15array);
 *)
 
 implementation
-
-uses
-  strutils, sysutils, pwfileutil, pwfputil;
+uses strutils, sysutils, pwfileutil, pwfputil;
 
 type
   TGroup = record
@@ -138,14 +143,12 @@ end;
 
 { display simple message }
 procedure NoteLn(const s: string);
-begin
-  writeln('Note: ', s);
+begin writeln('Note: ', s);
 end;
 
 { warn with polite message (no halt) }
 procedure WarnLn(const s: string);
-begin
-  writeln('Warning: ', s);
+begin writeln('Warning: ', s);
 end;
 
 { halt program with polite message }
@@ -166,9 +169,14 @@ begin
 end;
 
 procedure WriteSeparator1;
-begin                                                          
+begin
   writeln('-------------------------------------------------------------------------------');
-end;                               
+end;                
+
+procedure WriteDoneSeparator;
+begin 
+  writeln('------------FINISHED---------------FINISHED---------------FINISHED-------------');
+end;                
 
 { returns build group }
 function Group: str15;
@@ -203,7 +211,7 @@ begin
   if group = 'all' then result:= true;   
 end;
 
-{ checks if we are running "clean" process }
+{ checks if we are running a "clean" }
 function Cleaning: boolean;
 begin
   result:= false;
@@ -231,50 +239,68 @@ procedure ShowHelp;
   end;
 
 begin
-  // only show help if no groups
-  if paramcount > 0 then exit;
-  WriteSeparator1;
-  writeln('HELP: build <group> <flag>');
+  writeln('PowBuild by Z505 --> Syntax: build <group> <optional flag>');
+  writeln('Type in "build help" to receive extended help information');
   writeln;
-  writeln('  Custom Groups:');
-  if VisibleGroups = nil then HaltErr('Build author must set visible bulid groups.');
-  ShowLns(VisibleGroups);
-  writeln('  General Groups:');
+  if VisibleGroups = nil then HaltErr('Build author didn''t set visible bulid groups.');
+  writeln(' Main Build Groups:');
   ShowLns(Defgroups);
-  writeln('  Flags:');
+  writeln(' Custom Build Groups:');
+  ShowLns(VisibleGroups);
+  writeln(' Flags:');
   ShowLns(DefFlags);
   writeln;
-  writeln('Example: build default     (the usual build)');
-  writeln('         build df          (alias for default, short & easy)');
-  writeln('         build all         (compile all builds, defines, may take long)');
-  writeln('         build all clean   (just delete .ppu/.a and other junk)');
-  writeln('         build all rebuild (clean first, then build project)');
-  writeln('         build foobar      (user defined build)');
-  writeln('         build df clean       (etc.)');
-  writeln('         build df rebuild     (etc.)');
-  writeln('         build foobar clean   (etc.)');
-  writeln('         build foobar rebuild (etc.)');
-  WriteSeparator1;
+  writeln('i.e: build default        (the usual build)');
+  writeln('     build df             (df is an alias for "default")');
+  writeln('     build df clean       (clean all .ppu/.a files)');
+  writeln('     build df rebuild     (builds using -B, also cleans .ppu/.a)');
+  writeln('     build all            (compile all build groups, may take long)');
+  writeln('     build all clean      (clean .ppu/.a files of all build groups)');
+  writeln('     build all rebuild    (cleans all build groups, and builds using -B');
+  writeln('     build foobar         (user defined build)');
+  writeln('     build foobar clean   (etc. etc. etc.)');
   Halt;
 end;
 
-procedure ShowHelp2;
+procedure ShowMissingGroupHelp;
 begin
   WriteSeparator1;
-  writeln('HELP: target you specified was not found in this build system');
+  writeln('HELP: group (target) you specified was not found in this build system');
   WriteSeparator1;
   Halt;
 end;
 
+procedure ShowExtendedHelp;
+begin
+  writeln('EXTENDED HELP:');
+  writeln;
+  writeln('  PowBuild is similar to FpMake or Ruby Rake (worse or better!)');
+  writeln('  More powerful than GNU make, as build files are true fpc programs');
+  writeln('  A "build file" is a fpc program with pwbuildutil in uses clause');
+  writeln('  A build file can use any unit in uses (sysutils, pwdirutil, etc.)');
+  writeln;
+  writeln('  See an example "build.pp" file in any directory.');
+  writeln('  Once a build.pp file is found, compile it: "fpc build".');
+  writeln('  Then run "build" (help) or "build default" (builds default group)');
+  writeln;
+  writeln('  A build.pp file must set "visible groups" for the users.');
+  writeln('  Build files are "domain specific fpc programs" since they fill a niche');
+  writeln;
+  writeln('  A "group" is what you may know as a "target" from make files');
+  writeln('  To build a specific group, type "build yourgroup"');  
+  writeln;
+  writeln('...more <enter>');  
+  readln;
+  WriteSeparator1;
+  ShowHelp;
+end;
 
 function AllGroupNames: str15array;
 var i: int32;
 begin
   if length(groups) < 1 then exit;
   setlength(result, length(groups));
-  for i:= low(groups) to high(groups) do begin
-    result[i]:= groups[i].opts.name;
-  end;  
+  for i:= low(groups) to high(groups) do result[i]:= groups[i].opts.name;
 end;
 
 { ensures targets are setup right }
@@ -293,12 +319,16 @@ procedure CheckGroups;
     for i2:= low(groups) to high(groups) do begin
       if groups[i2].opts.name = group() then inc(found);
     end;
-    if found < 1 then ShowHelp2;
+    if found < 1 then ShowMissingGroupHelp;
   end;
 
 begin
   if paramcount < 1 then ShowHelp;
-   // only find groups in array if there are any specified at commandline
+  if (paramcount = 1) then if paramstr(1) = 'help' then begin
+    ShowExtendedHelp;
+    exit;
+  end;
+   // only find groups in array if any specified at commandline
   if paramcount > 0 then FindGroups;
 end;
 
@@ -330,7 +360,7 @@ var len: int32;
 begin      
   if s = '' then exit;
   len:= length(a);
-  setlength(a, len + 1);
+  setlength(a, len+1);
   a[len]:= s;
 end;
 
@@ -378,13 +408,19 @@ begin
   AstrArrayAdd(opts.intern.incpaths, s);
 end;
 
-{ adds extra compiler option,  example -Sd -Whatever -Blah }
+{ adds extra compiler option,  i.e. -Sd  -Whatever  -blah }
 procedure AddExtraOpt(var opts: TFpcOptions; s: astr);
 begin
   opts.extra:= opts.extra + ' ' + s;
 end;
 
+{ adds -dSOMEDEFINE }
+procedure AddDefine(var opts: TFpcOptions; s: astr);
+begin
+  AstrArrayAdd(opts.intern.defines, s);
+end;
 
+{ ... reset settings in record ...............................................}
 procedure ResetExtraOpts(var opts: TFpcOptions);
 begin
   opts.extra:= '';
@@ -400,15 +436,12 @@ begin
   AstrArrayReset(opts.intern.incpaths);
 end;
 
-procedure AddDefine(var opts: TFpcOptions; s: astr);
-begin
-  AstrArrayAdd(opts.intern.defines, s);
-end;
-
 procedure ResetDefines(var opts: TFpcOptions);
 begin
   AstrArrayReset(opts.intern.defines);
 end;
+{ ............................................................................}
+
 
 { delete .PPU/.A files a.k.a. "unit crap" }
 procedure CleanUnitCrap(const path: astr);
@@ -417,7 +450,7 @@ const
 var i, problems: int32;
 begin
   problems:= 0;
-  noteln('Removing files from dir: ' + path);
+  noteln('Cleaning: removing files from dir: ' + path);
   for i:= low(masks) to high(masks) do begin
     { warn if deletion of all files unsucessful }
     if not DelFiles(path, masks[i]) then inc(problems);
@@ -441,12 +474,12 @@ var allopts: astr = '';
 
   procedure AddStrArrayOpts;
 
-    procedure Add(prefix: astr; arr: AstrArray);
-    var i: longword;
+    procedure Add(prefix: astr; a: AstrArray);
+    var i: int32;
     begin
-      if length(arr) > 0 then
-        for i:= low(arr) to high(arr) do if length(arr[i]) > 0 then
-          AddOpts(prefix, arr[i]);      
+      if length(a) > 0 then
+        for i:= low(a) to high(a) do if length(a[i]) > 0 then
+          AddOpts(prefix, a[i]);      
     end;
 
   begin
@@ -487,8 +520,7 @@ begin
   if opts.progbindir <> '' then begin
     targdir:= opts.dir + opts.progbindir + FpcTargetDir();
     opts.intern.progtargetdir:= targdir;
-    if not ForceDir(targdir) then 
-      HaltErr('error creating directory: '+targdir); 
+    if not ForceDir(targdir) then HaltErr('error creating folder: '+targdir); 
   end;
 
   AddOpts('-FU', opts.intern.craptargetdir);
@@ -531,7 +563,7 @@ begin
   if path[len] <> SLASH then path:= path + SLASH;
 end;
 
-
+{ retrieve location of fpc path on drive }
 procedure GetFpcPath(var fpcpath: astr);
 var found: astr;
 begin
@@ -583,7 +615,7 @@ begin
   madeopts:= makeopts(opts);
   if not opts.Compile then exit; // just clean or do other tasks
   path:= opts.dir+srcunit;
-  writeln('>>> COMPILING ', path, ' >>>');
+  writeln('>>> COMPILING: ', path);
   result:= Compile(path, madeopts, opts.FpcVersion, opts.IgnoreErr);
 end;
 
@@ -591,12 +623,15 @@ procedure CompileMany(var paths: TPaths; var opts: TFpcOptions; ShowSeparator: b
 var i: int32;
 begin
   if paths.count < 1 then exit;
-  writeln('>>>>>> PROCESSING GROUP ', opts.Name, ' >>>>>>');
+  WriteSeparator1;
+  writeln('----- PROCESSING GROUP: ', opts.Name, ' -----');
+  WriteSeparator1;
   for i:= low(paths.items) to high(paths.items) do begin
     opts.dir:= paths.items[i].path;
     Compile(paths.items[i].fname, opts);
-    if ShowSeparator then WriteSeparator; // shows ----------- lines
+    if ShowSeparator then WriteSeparator1; // shows ----------- lines
   end;
+  WriteDoneSeparator; // finishing separator :o)
 end;
 
 procedure CompileMany(var paths: TPaths; var opts: TFpcOptions);
