1de681d1e53e7d109f1ec7af7f6a4b2f1b023d1c
adsense@z505.com <adsense@z505.com@8899f2f9-8946-0410-bedb-cd5a53bd3c4f>
2008-06-17 15:56:28 +0000

    variable naming cleanup

--- dev/tools/aservia/aservia.dpr
+++ dev/tools/aservia/aservia.dpr
@@ -13,9 +13,9 @@ uses
 
 // turn these on for more verbose reporting
 // TODO: put in a config file option
-const LOGGING_ON = false;
+const LOG_ON = false;
       MESSAGES_ON = false;   
-      DEBUGGING_ON = false;   
+      DEBUG_ON = false;   
 
 // NOTE: MUST BE HTTP/1.0 - DOESN'T SUPPORT 1.1 CHUNK ENCODING, ETC.
 const
@@ -30,61 +30,39 @@ const
       REMOTE_ADDR  = 'REMOTE_ADDR';
       THREAD_LOOP_SLEEP = 10;
 
+// globals prefixed with g
 var
-  server   : TzServer;
-  str1     : astr;
-  critical : TRTLCriticalSection;
-  fl       : text;
-  mimecfg, vhostcfg: TCfgFile;  
-  logflname: astr;
-  filedir  : astr;
-  ip       : astr;
-  port     : word;
-  newlog   : boo;
-  idxpg: astr; // default index page
-  error404 : astr;
-  error403 : astr;
-  blacklist: array of astr;
-  needStopServer: boo;
-  handle   : int32;
+  gServer: TzServer;     // main server instance
+  gPort: word;           // server port
+  gIp: astr              //    ... ip
+  gIdxPg,                // default index page  
+  gError404, gError403,  // error strings
+  gFileDir: astr;        // path for files to serve
+  gLogFname: astr        // log file name
+  gLog: text;            // log file record
+  gHandle: int32;
+  gMimeCfg, 
+  gVhostCfg: TCfgFile;  
+  gNewLog: boo;
+  gCritical: TRTLCriticalSection;
+  gBlacklist: array of astr;
+  gNeedStopServer: boo;
+
 
 { debugging to console }
-procedure dbugln(s: astr); overload;   
-begin if DEBUGGING_ON then writeln('DEBUG: ', s); 
-end;
+procedure dbugln(s: astr);   begin if DEBUG_ON then writeln('DEBUG: ', s); end;
+procedure dbugln(s1,s2:astr);begin dbugln(s1+s2); end;
 
-procedure dbugln(s1, s2: astr);        
-begin dbugln(s1+s2); 
-end;
+procedure logln(var t:text; s:astr); begin if LOG_ON then writeln(t, s); end;
+procedure logln(var t: text); begin logln(t, ''); end;
 
-procedure logln(var t: text; s: astr);  
-begin if LOGGING_ON then writeln(t, s); 
-end;
+{ write a line to console if messages on }
+procedure msgln(s1: astr);          begin if MESSAGES_ON then writeln(s1); end;
+procedure msgln(s1,s2: astr);       begin msgln(s1+s2); end;
+procedure msgln(s1,s2,s3: astr);    begin msgln(s1+s2+s3); end;
+procedure msgln(s1:astr; i:int32);  begin msgln(s1+i2s(i)); end;
 
-procedure logln(var t: text);  
-begin logln(t, ''); 
-end;
-
-
-{ write a line to console }
-
-procedure msgln(s1: astr);        
-begin if MESSAGES_ON then writeln(s1); 
-end;
-
-procedure msgln(s1,s2: astr);        
-begin if MESSAGES_ON then writeln(s1,s2); 
-end;
-
-procedure msgln(s1,s2,s3: astr);     
-begin if MESSAGES_ON then writeln(s1,s2, s3); 
-end;
-
-procedure msgln(s1: astr; i: int32); 
-begin if MESSAGES_ON then writeln(s1,i); 
-end;
-
-{ always write a note even if MESSAGES_ON is off }
+{ write a note even if MESSAGES_ON is off }
 procedure noteln(s1: astr);            begin  writeln(s1); end;
 procedure noteln(s1,s2: astr);         begin  writeln(s1,s2); end;
 procedure noteln(s1,s2,s3: astr);      begin  writeln(s1,s2,s3); end;
@@ -94,8 +72,7 @@ procedure DeleteTrailingHttpVersion(var s: astr);
 var found: int32;
 
   procedure removestr;
-  begin
-    if found > 0 then delete(s, found, (length(s)-found) +1);  
+  begin if found > 0 then delete(s, found, (length(s)-found) +1);  
   end;
 
 begin
@@ -114,17 +91,17 @@ begin
   s:= '';
   repeat
     readln(s);
-    if (s = 'q') or (s = 'quit') then needStopServer:= true;
+    if (s = 'q') or (s = 'quit') then gNeedStopServer:= true;
     sleep(800);
-  until needStopServer;
+  until gNeedStopServer;
   noteln(str_close);
-  server.Stop;
+  gServer.stop;
   result:= 0;
 end;
   
 function parceRequest(req: astr): astr;
   { wrapper for pos() }
-  function found(sub, instr: astr): bln;
+  function found(sub, instr: astr): boo;
   begin
     result:= false;
     if pos(sub, instr) <> 0 then result:= true;
@@ -159,7 +136,7 @@ begin
       result:= strings[i];
       // trim first 'POST ' part
       delete(result, 1, 5);
-      filedir := '';
+      gFileDir := '';
       postd := copy(req, pos(#13#10#13#10, req), length(req));
       postd := StringReplace(postd, '&', ';', [rfReplaceAll]);
       {$ifdef mswindows}postd := StringReplace(postd, '%', '%%', [rfReplaceAll]);{$endif}
@@ -173,7 +150,7 @@ begin
       if pos(':', strings[i]) <> 0 then
         strings[i] := copy(strings[i], 1, pos(':', strings[i]) - 1);
       addEnv('HTTP_HOST', strings[i]);
-      filedir := strings[i];
+      gFileDir := strings[i];
     end else if found('Referer:', strings[i]) then begin
       delete(strings[i], 1, 9);
       addEnv('HTTP_REFERER', strings[i]);
@@ -181,48 +158,45 @@ begin
   end;
 
   DeleteTrailingHttpVersion(result);
-  if (result = '') or (result[length(Result)] = '/') then result += idxpg;
+  if (result = '') or (result[length(result)] = '/') then result += gIdxPg;
 end;
   
-function gettype(fname: astr): astr;
-var ext  : astr;
-    def  : astr;
+function getType(fname: astr): astr;
+var ext, def: astr;
 begin
-  def:= mimecfg.getOpt('default', 'application/force-download');;
-  ext:= LowerCase(ExtractFileExt(fname));
+  def:= gMimeCfg.getOpt('default', 'application/force-download');;
+  ext:= lowerCase(ExtractFileExt(fname));
   delete(ext, 1, 1);
-  Result := mimecfg.getOpt(ext, def);
+  result := gMimeCfg.getOpt(ext, def);
 end;
 
-function getFile(fname: astr; errcode: astr = '200 OK'; 
-                 query: astr = ''): astr;
+function getFile(fname:astr; errcode:astr = '200 OK'; query:astr = ''): astr;
 var
-  fl: file;
-  buf: astr;
+  F: file;
   count: int32;
-  filetype: astr;
+  buf, filetype: astr;
   parameters: astr = '';
 begin
   filetype := gettype(fname);
   if filetype <> 'execute/cgi' then
   begin
     setLength(buf, 4096);
-    Result := '';
-    assign(fl, fname);
-    reset(fl, 1);
+    result := '';
+    assign(F, fname);
+    reset(F, 1);
     result:= HTTP_VERSION+' ' + errcode + #13#10 + 
              str_server + #13#10 +
             'MIME-version: 1.0'#13#10 +
             'Allow: GET, POST'#13#10 +
             'Content-type: ' + filetype + #13#10 +
-            'Content-length: ' + IntToStr(FileSize(fl)) + #13#10 + 
+            'Content-length: ' + IntToStr(FileSize(F)) + #13#10 + 
              #13#10;
-    while not eof(fl) do begin
-      blockRead(fl, pointer(buf)^, 4096, count);
+    while not eof(F) do begin
+      blockRead(F, pointer(buf)^, 4096, count);
       if count < 4096 then buf := copy(buf, 1, count);
       result += buf;
     end;  
-    Close(fl);
+    Close(F);
   end else begin
     if query <> '' then begin
       count := pos('?', query);
@@ -232,7 +206,7 @@ begin
     parameters := StringReplace(parameters, '%', '%%', [rfReplaceAll]);
    {$endif}
     parameters := StringReplace(parameters, '&', ';', [rfReplaceAll]);
-    Result := HTTP_VERSION+' '+errcode+#13#10 
+    result := HTTP_VERSION+' '+errcode+#13#10 
               + command(fname + ' "' + parameters + '"');
   end;
 end;  
@@ -240,163 +214,151 @@ end;
 function request(p: pointer): int32;
 var path: astr;
 
-  function SlashDots: bln;
+  function SlashDots: boo;
   begin
     result:= false;
-    if pos('/../', ExtractRelativepath(filedir, path)) <> 0 then result:= true; 
+    if pos('/../', ExtractRelativepath(gFileDir, path)) <> 0 then result:= true; 
   end;
 
 var
-  deny: boo;
+  deny: boo = false;
+  str1: astr = '';
 
-  procedure Log;
+  procedure log;
   begin
-    if not LOGGING_ON then exit;
+    if not LOG_ON then exit;
    {$I-}
-    Assign(fl, logflname); Append(fl); 
-    if IOResult <> 0 then Rewrite(fl);
-    logln(fl, str_requestfrom+ip+'; '{, DateTimeToStr(Date), ', ', TimeToStr(Time)});
-    logln(fl, str1);
-    if deny then logln(fl, str_denied);
-    close(fl);
+    Assign(gLog, gLogFname); Append(gLog); 
+    if IOResult <> 0 then Rewrite(gLog);
+    logln(gLog, str_requestfrom+gIp+'; '{, DateTimeToStr(Date), ', ', TimeToStr(Time)});
+    logln(gLog, str1);
+    if deny then logln(gLog, str_denied);
+    close(gLog);
    {$I+}
   end;
 
-var fname, name, ip: astr;
-    i: int32;
-    pInt: int32;
+var fname, name, gIp: astr;
+    i, data: int32;
 begin
-  deny:= false;
-  pInt:= int32(p^);
-  enterCriticalSection(critical);
+  // incoming pointer converted to strong type
+  data:= int32(p^);
+  enterCriticalSection(gCritical);
   try
     clearEnv;
-    server.select(pInt);
-    str1:= server.sRead;
+    gServer.select(data);
+    str1:= gServer.sRead;
     msgln(str_request{, DateTimeToStr(Date), ', ', TimeToStr(Time)});
     msgln(str1);
-    ip:= server.getip(pInt);
-    addEnv(REMOTE_ADDR, ip);
+    gIp:= gServer.getIp(data);
+    addEnv(REMOTE_ADDR, gIp);
     addEnv(SERV_SOFT, str_server);
     i:= 0;
-    while (not deny) and (i < length(blacklist)) do begin
-      if ip = blacklist[i] then begin 
+    while (not deny) and (i < length(gBlacklist)) do begin
+      if gIp = gBlacklist[i] then begin 
         deny:= true; 
         msgln(str_denied); 
       end;
       inc(i);
     end;
     dbugln('Checked blacklist');
-    if deny then server.sWrite(getFile(error403, ERR_403_MSG)) 
+    if deny then gServer.sWrite(getFile(gError403, ERR_403_MSG)) 
     else
     if str1 <> '' then
     begin
       fname := parceRequest(str1);
-      if (filedir = '') or (filedir = ip) then 
-        filedir:= vhostcfg.getOpt('default', '')
+      if (gFileDir = '') or (gFileDir = gIp) then 
+        gFileDir:= gVhostCfg.getOpt('default', '')
       else begin
-        filedir:= vhostcfg.getOpt(filedir, vhostcfg.getOpt('default', ''));
-        filedir:= excludeTrailingPathDelimiter(filedir);
+        gFileDir:= gVhostCfg.getOpt(gFileDir, gVhostCfg.getOpt('default', ''));
+        gFileDir:= excludeTrailingPathDelimiter(gFileDir);
       end;
-      addEnv(DOC_ROOT, filedir);
+      addEnv(DOC_ROOT, gFileDir);
       name:= fname;
       if pos('?',fname) > 0 then name:= copy(fname,1,pos('?',fname) - 1);
-      path:= filedir + name; 
+      path:= gFileDir + name; 
       xpath(path);
-      if (not fileThere(path)) and dirExists(path) then name:= name+'/'+idxpg;
+      if (not fileThere(path)) and dirExists(path) then name:= name+'/'+gIdxPg;
       addEnv(SCRIPT_NAME, name);
-      path:= filedir + name;
+      path:= gFileDir + name;
       xpath(path); // cross platform slashes
       if FileThere(path) then begin
         if SlashDots then begin
-          server.sWrite(getFile(error403, ERR_403_MSG));
+          gServer.sWrite(getFile(gError403, ERR_403_MSG));
           deny:= true;
         end else 
-          server.sWrite(getFile(path, '200 OK', fname))
+          gServer.sWrite(getFile(path, '200 OK', fname))
       end else begin
-        server.sWrite(getFile(error404, ERR_404_MSG));
+        gServer.sWrite(getFile(gError404, ERR_404_MSG));
       end;
     end;
     dbugln('Before log() procedure');
     log;
 
   finally
-    dbugln('Disconnecting: '+i2s(pInt));
-    server.disconnect(pInt);
-    leaveCriticalSection(critical);
+    dbugln('Disconnecting: '+i2s(data));
+    gServer.disconnect(data);
+    leaveCriticalSection(gCritical);
     dbugln('Left Critical Section');
   end;
   result:= 0;
-  // IF THIS ENDTHREAD IS NOT PLACED HERE, THEN LINUX KEEPS OPENING THREADS
-  // TO A MAXIMUM OF 380 AND IT CAUSES A CONTINUAL LEAKAGE
-  EndThread;
+  // IF ENDTHREAD IS NOT PLACED HERE, THEN LINUX KEEPS OPENING THREADS TO A 
+  // MAXIMUM OF 380 AND CAUSES A CONTINUAL LEAKAGE
+  endThread;
   dbugln('Ended thread');
 end;
 
 procedure setupLog;
 begin
-  if not LOGGING_ON then exit;
+  if not LOG_ON then exit;
  {$I-}
-  assign(fl, logflname);
-  if not newlog then Append(fl);
-  if newlog or (IOResult <> 0) then Rewrite(fl);
-  logln(fl, str_server+#13#10);
-  logln(fl, str_runserver{, DateTimeToStr(Date), ', ', TimeToStr(Time)});
-  logln(fl, str_socket+ip+':'+i2s(port));
-  logln(fl);  
-  close(fl);
+  assign(gLog, gLogFname);
+  if not gNewLog then append(gLog);
+  if gNewLog or (IOResult <> 0) then rewrite(gLog);
+  logln(gLog, str_server+#13#10);
+  logln(gLog, str_runserver{, DateTimeToStr(Date), ', ', TimeToStr(Time)});
+  logln(gLog, str_socket+gIp+':'+i2s(gPort));
+  logln(gLog);  
+  close(gLog);
  {$I+}
 end;
 
 { start thread to detect keyboard q/quit at console }
 procedure beginNeedStopThread;
-var id: TTHreadId = 0; 
-    //err: dword = 0;
+var id: TThreadId = 0; 
 begin
   //id:= BeginThread(nil,DefaultStackSize,@needStop,nil,0,dummy);
-   { UNIX and WINDOWS return different results (fpc bug) }
   id:= beginThread(@needStop);
+  { UNIX and WINDOWS return different results (fpc bug) }
   if id <> 0 then dbugln('(needStopThread) beginThread Result#: '+i2s(id));
-//  err:= suspendThread(id);
-//  if err <> 0 then dbugln('suspendThread Error#: ', err);
-//  err:= 0;
-//  err:= resumeThread(id);
-//  if err > 0 then dbugln('resumeThread Error#: '+i2s(err));
 end;
 
 { start connection threads }
 procedure beginRequestThread;
 var id: TTHreadId = 0; 
-    //err: dword = 0;
 begin
   //id:= BeginThread(nil,DefaultStackSize,@needStop,nil,0,dummy);
-  id:= beginThread(@request, @handle);
+  id:= beginThread(@request, @gHandle);
   { UNIX and WINDOWS return different results (fpc bug) }
   if id <> 0 then dbugln('(requestThread) beginThread Result#: '+i2s(id));
-//  err:= suspendThread(id);
-//  if err <> 0 then dbugln('suspendThread Error#: ', err);
-//  err:= 0;
-//  err:= resumeThread(id);
-//  if err > 0 then dbugln('resumeThread Error#: '+i2s(err));
 end;
 
 { main server connections loop for all requests }
 procedure runThreadLoop;
 begin
   beginNeedStopThread;
-  initCriticalSection(critical);
+  initCriticalSection(gCritical);
   repeat
     msgln(str_wait);
-    handle:= server.connect;
-    if (not needStopServer) and (handle >= 0) then begin
+    gHandle:= gServer.connect;
+    if (not gNeedStopServer) and (gHandle >= 0) then begin
       msgln(str_connection);
       beginRequestThread;
     end else begin
-      dbugln('runThreadLoop Handle: '+i2s(handle));
+      dbugln('runThreadLoop Handle: '+i2s(gHandle));
     end;
     sleep(THREAD_LOOP_SLEEP); 
-  until needStopServer;
-  doneCriticalSection(critical);
+  until gNeedStopServer;
+  doneCriticalSection(gCritical);
 end;
 
 procedure errLn(const msg: astr); 
@@ -407,46 +369,46 @@ procedure errLn;
 begin errLn('');
 end;
 
-procedure CreateCfgAndServer;
+procedure createCfgAndServer;
   { retreive configuration file info, or use defaults if not avail }
   procedure GetMainCfg;
   var othercfg: TCfgFile;
   begin
     othercfg:= TCfgFile.create('config.cfg');
-      ip        := othercfg.getOpt('ip',        '127.0.0.1');
-      port      := othercfg.getOpt('port',      80);
-      newlog    := othercfg.getOpt('deletelog', false);
-      logflname := othercfg.getOpt('logfile',   'connections.log');
-      idxpg     := othercfg.getOpt('index',     'index.html');
-      error404  := othercfg.getOpt('error404',  'error404.html');
-      error403  := othercfg.getOpt('error403',  'error403.html');
+      gIp        := othercfg.getOpt('ip',        '127.0.0.1');
+      gPort      := othercfg.getOpt('port',      80);
+      gNewLog    := othercfg.getOpt('deletelog', false);
+      gLogFname := othercfg.getOpt('logfile',   'connections.log');
+      gIdxPg     := othercfg.getOpt('index',     'index.html');
+      gError404  := othercfg.getOpt('error404',  'error404.html');
+      gError403  := othercfg.getOpt('error403',  'error403.html');
     othercfg.free; othercfg:= nil;
 
     othercfg:= TCfgFile.create('blacklist.cfg');
-      blacklist:= othercfg.getAllOpts;
+      gBlacklist:= othercfg.getAllOpts;
     othercfg.free; othercfg:= nil;
   end;
 
 begin
-  GetMainCfg;
-  mimecfg:= TCfgFile.create('mime.cfg');
-  vhostcfg:= TCfgFile.create('vhost.cfg');
-  server:= TzServer.Create;
+  getMainCfg;
+  gMimeCfg:= TCfgFile.create('mime.cfg');
+  gVhostCfg:= TCfgFile.create('vhost.cfg');
+  gServer:= TzServer.Create;
 end;
 
 { cleanup }
-procedure FreeCfgAndServer;
+procedure freeCfgAndServer;
 begin
-  vhostcfg.Free; vhostcfg:= nil;
-  mimecfg.Free; mimecfg:= nil;
-  server.Free; server:= nil;
+  gVhostCfg.Free; gVhostCfg:= nil;
+  gMimeCfg.Free; gMimeCfg:= nil;
+  gServer.Free; gServer:= nil;
 end;
 
-procedure ErrCantConnect;
+procedure errCantConnect;
 begin
   ErrLn;
   ErrLn('Can''t connect to address or port.');
-  ErrLn('The ip:port you are using is '+ip+':'+inttostr(port));
+  ErrLn('The ip:port you are using is '+gIp+':'+inttostr(gPort));
   ErrLn('Tip: make sure another server is not running.');
   ErrLn('Error # '+ {$ifdef windows}inttostr(GetLastError){$endif}
                     {$ifdef unix}inttostr(fpGetErrNo){$endif}
@@ -455,20 +417,20 @@ begin
     fpc 2.2.0. See http://bugs.freepascal.org/view.php?id=10205 }
 
   // cleanup, then kill
-  FreeCfgAndServer; Halt;
+  freeCfgAndServer; Halt;
 end;
 
 { initiates logs, runs thread loop }
-procedure RunServer;
+procedure runServer;
 var inited: boo;
 begin
   noteln(str_server);
   noteln(str_qcom, #13#10);
   noteln(str_runserver{, DateTimeToStr(Date), ', ', TimeToStr(Time)});
   createCfgAndServer;
-  inited:= server.initConnection(ip, port);
+  inited:= gServer.initConnection(gIp, gPort);
   if inited then begin
-    noteln(str_socket, ip, ':', i2s(port));
+    noteln(str_socket, gIp, ':', i2s(gPort));
     setupLog;
     runThreadLoop;
   end else 
@@ -479,7 +441,7 @@ end;
 
 ///////////////////////////////////////////////////////////////////////////////
 begin
-  needStopServer:= false;
+  gNeedStopServer:= false;
   runServer;
 end.
 ///////////////////////////////////////////////////////////////////////////////
