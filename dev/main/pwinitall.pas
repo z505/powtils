d0a842ce51c3bc5175dff5f11d8c6d8f818f746e
adsense@z505.com <adsense@z505.com@8899f2f9-8946-0410-bedb-cd5a53bd3c4f>
2008-08-07 00:00:30 +0000

    pwmain.Fini() must be called in PWInitAll finalization section

--- dev/main/pwinitall.pas
+++ dev/main/pwinitall.pas
@@ -21,6 +21,9 @@ initialization
   // initializes sessions, config, based on plugin units above, and also
   // whatever pwmain must initialize such as headers, buffer, etc
   Init;
+
+finalization
+  Fini;
 end.
 
 
