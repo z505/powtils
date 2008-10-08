8a4c63c021b55366fc6cd438b603c40c3af3a7b8
camarada_aldo <camarada_aldo@yahoo.com.br>
2008-10-08 14:34:57 +0000

    debuging pasjs

--- dev/tools/PasJS/testparsing.pas
+++ dev/tools/PasJS/testparsing.pas
@@ -18,14 +18,23 @@ end;
 
 procedure test;
 var c : integer;
-  b: boolean;
+  b: integer;
+  a: integer;
 Begin
-  while b do
+  while b and c or not b do
     for c := 1 to 100 do
     begin
       writeint(c);
       b := c = 10;
     end;
+  if b = c then
+  repeat
+    a := a + 1;
+  until a = 10
+  else
+  repeat
+    a := a - 1;
+  until a = 0;
 End;
 
 function test2: boolean;
